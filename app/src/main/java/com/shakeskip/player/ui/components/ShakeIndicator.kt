dcbbcfb899c3fde85f1154433a7e8750ae00d40e
UltimateGregg <garrettpace591@gmail.com>
2025-10-29 00:44:40 -0500

    Fix Modifier.offset import for Compose

--- app/src/main/java/com/shakeskip/player/ui/components/ShakeIndicator.kt
+++ app/src/main/java/com/shakeskip/player/ui/components/ShakeIndicator.kt
@@ -12,7 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.offset
+import androidx.compose.foundation.layout.offset
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.text.font.FontWeight
